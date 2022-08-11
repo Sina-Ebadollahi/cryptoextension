b768dc50f6ece1ab493a202b134a5f3f1a898a34
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-08-11 21:32:19 +0430

    solving minor issues and creating developing popup

---
+++ src/options/Components/PopupComponents/Popup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Popup() {
+  return (
+    <div style={{minHeight: "600px", minWidth: "600px"}} className="container row bg-dark">
+        
+    </div>
+  )
+}
